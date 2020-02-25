c4046657bdc101acef01773b42b30e402fdb4321
imida <imlundstad@gmail.com>
2020-02-25 21:42:44 +0100

    started on data

---
+++ database/dbbergen_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO `bergendb`.`Home`
+(`idHome`,
+`Title`,
+`Text`,
+`Sound`)
+VALUES
+(1,
+'Velkommen',
+'bla bla',
+'mwau');
+
+
+INSERT INTO `bergendb`.`Weather`
+(`idWeather`,
+`Title`,
+`Text`,
+`Video`)
+VALUES
+(1,
+'Vær og klima',
+'kommer snart',
+'film');
+
+
